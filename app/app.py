74ccc66e7f0befd236a65194e5b38e3d2c50b172
VictorMay07 <145171268+VictorMay07@users.noreply.github.com>
2024-10-03 14:05:11 -0600

    1234
    wertyui

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sistemacontable'
-app.config['MYSQL_PORT'] = 3306  
+app.config['MYSQL_PORT'] = 5000  
 
 mysql = MySQL(app)
 
