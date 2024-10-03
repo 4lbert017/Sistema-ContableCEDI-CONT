bc76da413a2b6a58c17b076ad57d88b50d3f2116
VictorMay07 <145171268+VictorMay07@users.noreply.github.com>
2024-10-03 13:58:07 -0600

    cambios 1
    cambios 1

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sistemacontable'
+app.config['MYSQL_PORT'] = 3306  
 
 mysql = MySQL(app)
 
